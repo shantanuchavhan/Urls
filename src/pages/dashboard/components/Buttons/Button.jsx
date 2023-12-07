60a482ae6e08245f182f7ae72bdaca9c1455593f
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 00:22:28 +0530

    commit

--- src/pages/dashboard/components/Buttons/Button.jsx
+++ src/pages/dashboard/components/Buttons/Button.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 const Button = ({ icon, label, onClick }) => {
   return (
     <button
-    className='p-2 flex items-center justify-between gap-2 py-1 px-4 border border-gray-300 rounded-lg'
+    className='w-full md:w-max flex  items-center justify-center md:justify-between shadow-lg gap-2 py-1 px-4 border border-gray-300 rounded-lg'
     style={{ borderRadius: "16px" }}
     onClick={onClick}
   >
-    {icon && <img className='h-4 w-4' src={icon} alt='' />}
+    {icon && <img className='h-6 w-6 lg:w-4 lg:h-4' src={icon} alt='' />}
     {label && <span className='hidden md:flex text-gray-600'>{label}</span>}
   </button>
+  
   
   );
 };
