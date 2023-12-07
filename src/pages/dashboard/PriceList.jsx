0c358bf3f04e2e6e03a3c51357fe832a083526c6
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-07 23:17:03 +0530

    commit

--- src/pages/dashboard/PriceList.jsx
+++ src/pages/dashboard/PriceList.jsx
@@ -12,20 +12,20 @@ const PriceList = () => {
     <div className='text-black flex flex-col gap-6 '>
       <div className=' md:flex   justify-between '>
         <div className='flex flex-col gap-2'>
-          <div className='flex items-center justify-between bg-white px-2 rounded-large'>
-            <input type="text" placeholder='Search article no' className='w-full outline-none' />
+          <div className='flex items-center justify-between bg-white px-2 rounded-large' style={{ borderRadius: "16px" }}>
+            <input type="text" placeholder='Search article no' className='w-full outline-none'  />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-blue-500">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
           </div>
-          <div className='flex items-center justify-between bg-white px-2 rounded-large'>
-            <input type="text" placeholder='Search product' className='w-full outline-none' />
+          <div className='flex items-center justify-between bg-white px-2 rounded-large' style={{ borderRadius: "16px" }}>
+            <input type="text" placeholder='Search product' className='w-full '  />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-blue-500">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
           </div>
         </div>
-      <div className='flex items-center justify-center gap-4 text-sm'>
+      <div className='flex items-center pt-4 justify-center gap-4 text-sm'>
                 <Button icon={addIcon} label='New Product' onClick={() => console.log('New Product')} />
                 <Button icon={printerIcon} label='Print List' onClick={() => console.log('Print List')} />
                 <Button icon={printerIcon} label='Advanced Mode' onClick={() => console.log('Advanced Mode')} />
