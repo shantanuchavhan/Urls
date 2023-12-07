7e02690a953e195cbdf50e0cd2123d4235a6213b
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-07 15:17:44 +0530

    commit

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 import style from './style.module.css'
 import Header from './components/Header/Header'
-
+import { Link } from 'react-router-dom'
 
 
 
@@ -11,6 +11,7 @@ const HomeLayout = () => {
   return (
     <div className={style.container}>
       <Header/>
+      <Link to="/dashboard">dashboard</Link>
       <div className='flex items-center justify-center px-[200px] py-7 text-white'>
         <Outlet />
       </div>
