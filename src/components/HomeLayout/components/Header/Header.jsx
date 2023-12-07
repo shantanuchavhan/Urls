b33c815fe3b68ab3221cc1a421638f89cf8fec24
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 01:51:17 +0530

    commit

--- src/components/HomeLayout/components/Header/Header.jsx
+++ src/components/HomeLayout/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import CountrySection from '../../../CountrySection/CountrySection'
 
 const Header = () => {
   return (
-    <div className='flex justify-between px-10 md:px-[100px] lg:px-[200px] py-7 text-white'>
+    <div className='flex justify-between px-10 md:px-[100px] lg:px-[200px] py-10 text-white'>
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
