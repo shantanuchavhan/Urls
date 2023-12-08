8a9a0dd57990b9d0ed334a8ef7c069faac22ecfd
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 21:18:00 +0530

    commit

--- src/pages/terms/Terms.jsx
+++ src/pages/terms/Terms.jsx
@@ -27,7 +27,7 @@ const Terms = () => {
   return (
     <div className="px-3 md:px-10 lg:px-20">
       <div className="text-center mt-2 flex flex-col gap-10">
-        <h1 className="text-white m-0 font-[700] text-2xl">Terms</h1>
+        <h1 className="text-white m-0 font-[700] text-3xl">Terms</h1>
         {loading ? (
           <LoadingSpinner />
         ) : (
@@ -41,9 +41,8 @@ const Terms = () => {
               className="bg-white px-10 py-10 text-center text-[14px] leading-7 font-[300] text-[#282b31] rounded-md"
               style={{ borderRadius: "16px" }}
             >
-              <p>
+              <p className='text-base'>
                 {terms}
-                
               </p>
             </div>
 
