faf066d1290ef116cf4deb6233600565b4318a4b
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 22:09:57 +0530

    commit

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -5,19 +5,25 @@ import style from './style.module.css'
 import Header from './components/Header/Header'
 import { Link } from 'react-router-dom'
 
+import bgImage from "../../images/bg-image.jpg"
 
 
 const HomeLayout = () => {
   return (
-    <div className={style.container}>
-      <Header/>
-      <div className='text-white flex items-center justify-center gap-4 text-lg'>
-        <Link to="dashboard/pricelist">PriceList</Link>
-        <Link to="/terms">terms</Link>
-        <Link to="/us">us</Link>
+    <div className="relative">
+      <div className='fixed'>
+          <img src={bgImage} alt="" />
       </div>
-      <div className='flex items-center justify-center py-7 px-2 md:px-[100px] lg:px-[200px]   text-white' >
-        <Outlet />
+      <div className='absolute top-0'>
+        <Header/>
+        <div className='text-white flex items-center justify-center gap-4 text-lg'>
+          <Link to="dashboard/pricelist">PriceList</Link>
+          <Link to="/terms">terms</Link>
+          <Link to="/us">us</Link>
+        </div>
+        <div className='flex items-center justify-center py-7 px-2 md:px-[100px] lg:px-[200px]   text-white' >
+          <Outlet />
+        </div>
       </div>
     </div>
   )
