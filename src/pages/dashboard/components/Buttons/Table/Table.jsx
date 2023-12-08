fd9eed343e2703bc4f2b21098d1415d7b1695b59
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 10:55:07 +0530

    commit

--- src/pages/dashboard/components/Buttons/Table/Table.jsx
+++ src/pages/dashboard/components/Buttons/Table/Table.jsx
@@ -1,45 +1,11 @@
-import React from 'react';
-import { useState,useEffect } from 'react';
-import {
-  Table as UITable,
-  TableHeader,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell,
-} from "../../../../../@/components/ui/table";
-
-
-
-// const generateRandomData = (count) => {
-//   const data = [];
-//   for (let i = 0; i < count; i++) {
-//     const randomArticleNo = Math.floor(Math.random() * 10000000000);
-//     const randomProductName = `Product ${i + 1}`;
-//     const randomInPrice = Math.floor(Math.random() * 100000);
-//     const randomPrice = Math.floor(Math.random() * 2000000);
-//     const randomUnit = ['Piece', 'Kg', 'Liter'][Math.floor(Math.random() * 3)];
-//     const randomInStock = Math.floor(Math.random() * 100);
-//     const randomDescription = `Description for Product ${i + 1}`;
-
-//     data.push([
-//       randomArticleNo,
-//       randomProductName,
-//       randomInPrice,
-//       randomPrice,
-//       randomUnit,
-//       randomInStock,
-//       randomDescription,
-//     ]);
-//   }
-//   return data;
-// };
-
-
-
+import React, { useState, useEffect } from 'react';
+import { Table as UITable, TableHeader, TableRow, TableHead, TableBody, TableCell } from "../../../../../@/components/ui/table";
+import LoadingSpinner from '../../../../../components/LoadingSpinner/LoadingSpinner';
 
 const Table = () => {
-  const [priceList,setPriceList]=useState([])
+  const [priceList, setPriceList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   useEffect(() => {
@@ -52,11 +18,11 @@ const Table = () => {
         const data = await response.json();
         setPriceList(data.message);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
     };
-
-
 
     const handleResize = () => {
       setScreenWidth(window.innerWidth);
@@ -72,7 +38,6 @@ const Table = () => {
     };
   }, []);
 
-
   const headers = [
     { label: 'Article No', icon: 'text-blue-300' },
     { label: 'Product/Service', icon: 'text-green-300' },
@@ -83,15 +48,13 @@ const Table = () => {
     { label: 'Description', icon: '' },
   ];
 
-  // Define a variable to determine the maximum number of columns based on screen width
   const maxColumns = screenWidth < 600 ? 2 : screenWidth < 800 ? 4 : 7;
 
-
   return (
-    <UITable >
-      <TableHeader >
+    <UITable>
+      <TableHeader>
         <TableRow>
-            {headers.slice(0, maxColumns).map((header, index) => (
+          {headers.slice(0, maxColumns).map((header, index) => (
             <TableHead key={index}>
               <div className='flex items-center gap-1 '>
                 <span>{header.label}</span>
@@ -106,22 +69,34 @@ const Table = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {priceList?.map((row, rowIndex) => (
-          
-          <TableRow key={rowIndex} >
-            {row.slice(0, maxColumns).map((cell, cellIndex) => (
-              
-              <TableCell key={cellIndex}>
-                <h1 className='text-[12px] border border-sky-200 rounded-lg p-1 px-2' style={{ borderRadius: "16px" }}>{cell}</h1>
-              </TableCell>
-            ))}
-            <div className='pl-4'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-400" >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                </svg>
-            </div>
+        {loading ? (
+          <TableRow>
+            <TableCell colSpan={maxColumns}>
+              <LoadingSpinner /> {/* Render your loading component here */}
+            </TableCell>
           </TableRow>
-        ))}
+        ) : error ? (
+          <TableRow>
+            <TableCell colSpan={maxColumns} className="text-red-500">
+              Error loading data: {error}
+            </TableCell>
+          </TableRow>
+        ) : (
+          priceList?.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {row.slice(0, maxColumns).map((cell, cellIndex) => (
+                <TableCell key={cellIndex}>
+                  <h1 className='text-[12px] border border-sky-200 rounded-lg p-1 px-2' style={{ borderRadius: "16px" }}>{cell}</h1>
+                </TableCell>
+              ))}
+              <div className='pl-4'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-400">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                </svg>
+              </div>
+            </TableRow>
+          ))
+        )}
       </TableBody>
     </UITable>
   );
