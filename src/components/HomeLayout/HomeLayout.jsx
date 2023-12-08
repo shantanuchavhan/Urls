05ffe2663a3b9e6726ca00a3ce2db10ff245f6fb
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 22:55:23 +0530

    commit

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-
-import style from './style.module.css'
-import Header from './components/Header/Header'
-import { Link } from 'react-router-dom'
-
-
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import style from './style.module.css';
+import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 
 const HomeLayout = () => {
   return (
     <div className={style.container}>
       <div className={style.move}>
-            <Header/>
-            <div className='text-white flex items-center justify-center gap-4 text-lg'>
-              <Link to="dashboard/pricelist">PriceList</Link>
-              <Link to="/terms">terms</Link>
-              <Link to="/us">us</Link>
-            </div>
-            <div className='flex items-center justify-center py-7 px-2 md:px-[100px] lg:px-[200px]   text-white' >
-              <Outlet />
-            </div>
+        <Header />
+        <div className={`${style.text} flex items-center justify-center gap-4 text-lg`}>
+          <Link to="dashboard/pricelist">PriceList</Link>
+          <Link to="/terms">terms</Link>
+          <Link to="/us">us</Link>
+        </div>
+        <div className={`flex items-center justify-center ${style.content}`}>
+          <Outlet />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeLayout
+export default HomeLayout;
