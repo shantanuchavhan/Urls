7ae705e147b20848dd962a0b82ca30c29358cb1e
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 22:11:18 +0530

    commit

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-import style from './style.module.css'
+
 import Header from './components/Header/Header'
 import { Link } from 'react-router-dom'
 
