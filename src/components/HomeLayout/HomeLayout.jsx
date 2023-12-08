e729aa612956ad80eb7168e7afb54ac7a8976efe
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 22:30:16 +0530

    commit

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -1,41 +1,26 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header/Header';
-import { Link } from 'react-router-dom';
-import bgImage from "../../images/bg-image.jpg";
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+import style from './style.module.css'
+import Header from './components/Header/Header'
+import { Link } from 'react-router-dom'
+
+
 
 const HomeLayout = () => {
   return (
-    <div className="relative">
-      <div className="fixed inset-0 z-1 overflow-hidden">
-        <img
-          src={bgImage}
-          alt=""
-          className="fixed-bg object-cover w-full h-full"
-          style={{ filter: 'brightness(0.7)' }} // Adjust brightness as needed
-        />
+    <div className={style.container}>
+      <Header/>
+      <div className='text-white flex items-center justify-center gap-4 text-lg'>
+        <Link to="dashboard/pricelist">PriceList</Link>
+        <Link to="/terms">terms</Link>
+        <Link to="/us">us</Link>
       </div>
-      <div className="fixed inset-0 z-0 overflow-hidden mt-20">
-        <img
-          src={bgImage}
-          alt=""
-          className="fixed-bg object-cover w-full h-full"
-          style={{ filter: 'brightness(0.7)' }} // Adjust brightness as needed
-        />
-      </div>
-      <div className="absolute z-2 top-0">
-        <Header />
-        <div className="text-white flex items-center justify-center gap-4 text-lg">
-          <Link to="dashboard/pricelist">PriceList</Link>
-          <Link to="/terms">Terms</Link>
-          <Link to="/us">Us</Link>
-        </div>
-        <div className="flex items-center justify-center py-7 px-2 md:px-[100px] lg:px-[200px] text-white">
-          <Outlet />
-        </div>
+      <div className='flex items-center justify-center py-7 px-2 md:px-[100px] lg:px-[200px]   text-white' >
+        <Outlet />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default HomeLayout;
+export default HomeLayout
