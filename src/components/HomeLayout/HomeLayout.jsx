85625e59d82b4c13b367acc812133c35fe8e5682
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 23:50:55 +0530

    commit

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -9,7 +9,9 @@ import { Link } from 'react-router-dom'
 
 const HomeLayout = () => {
   return (
-    <div className={style.container}>
+    <div >
+      <div className={style.container}></div>
+      <div className={style.content}>
       <Header/>
       <div className='text-white flex items-center justify-center gap-4 text-lg'>
         <Link to="dashboard/pricelist">PriceList</Link>
@@ -18,6 +20,7 @@ const HomeLayout = () => {
       </div>
       <div className='flex items-center justify-center py-7 px-2 md:px-[100px] lg:px-[200px]   text-white' >
         <Outlet />
+      </div>
       </div>
     </div>
   )
