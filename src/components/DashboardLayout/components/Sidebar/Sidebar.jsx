8aa1a5c6f83045026df0916735efb3fe3de4295e
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-08 23:22:57 +0530

    commit

--- src/components/DashboardLayout/components/Sidebar/Sidebar.jsx
+++ src/components/DashboardLayout/components/Sidebar/Sidebar.jsx
@@ -7,8 +7,13 @@ import { useState } from 'react';
 import dashboardSections from './dashboardSectionsLinks'; 
 import active from "../../../../images/active.png"
 
+import { useLocation } from 'react-router-dom';
+
 const Sidebar = () => {
-    const [activeSection,setActiveSection]=useState("")
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const lastWord = currentPath.split('/').filter(Boolean).pop();
+    const [activeSection,setActiveSection]=useState(lastWord||"")
     return (
       <div className='flex flex-col items-center justify-center py-3 gap-3 shadow-right text-gray-500 p-4'>
         <div className='border-b-2 border-indigo-500 w-full text-center'>
@@ -17,10 +22,10 @@ const Sidebar = () => {
         <div className='flex flex-col gap-3 '>
           {dashboardSections.map((menuItem, index) => (
             <Link to={`/dashboard/${menuItem.route}`} key={index} onClick={()=>{
-                setActiveSection((old)=>menuItem.label)
+                setActiveSection((old)=>menuItem.route)
             }}>
             <div className='flex gap-2 items-center relative'>
-                {activeSection === menuItem.label && <div className='h-3 w-3 absolute -left-5'><img src={active} alt="" /></div>}
+                {activeSection === menuItem.route && <div className='h-3 w-3 absolute -left-5'><img src={active} alt="" /></div>}
                 <div className='flex items-center gap-2'>
                 <img className='h-4 w-4' src={menuItem.icon} alt='' />
                 <h5 className='text-sm'>{menuItem.label}</h5>
