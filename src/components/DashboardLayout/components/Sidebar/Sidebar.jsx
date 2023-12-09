b8ec153bd9818711d56d66d8a2023b163a3d0919
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-09 10:26:26 +0530

    commit

--- src/components/DashboardLayout/components/Sidebar/Sidebar.jsx
+++ src/components/DashboardLayout/components/Sidebar/Sidebar.jsx
@@ -7,25 +7,28 @@ import { useState } from 'react';
 import dashboardSections from './dashboardSectionsLinks'; 
 import active from "../../../../images/active.png"
 
-
+import { useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
-  
-    const [activeSection,setActiveSection]=useState("")
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const lastWord = currentPath.split('/').filter(Boolean).pop();
+    const [activeSection,setActiveSection]=useState(lastWord||"")
     return (
-      <div className='flex flex-col items-center justify-center py-3 gap-3 shadow-right text-gray-500 p-4'>
+      <div className='flex flex-col  items-center justify-center py-3 gap-3 shadow-right text-gray-500 p-4'>
         <div className='border-b-2 border-indigo-500 w-full text-center'>
           <h2>Menu</h2>
         </div>
-        <div className='flex flex-col gap-3 w-screen '>
+        <div className='flex flex-col  gap-3'>
           {dashboardSections.map((menuItem, index) => (
             <Link to={`/dashboard/${menuItem.route}`} key={index} onClick={()=>{
-                setActiveSection((old)=>menuItem.label)
+                setActiveSection((old)=>menuItem.route)
             }}>
-            <div>
-            <div className='flex items-center gap-2'>
+            <div className='flex gap-2 items-center relative'>
+                {activeSection === menuItem.route && <div className='h-3 w-3 absolute -left-5'><img src={active} alt="" /></div>}
+                <div className='flex items-center gap-2'>
                 <img className='h-4 w-4' src={menuItem.icon} alt='' />
-                <h5 className=' text-black'>{menuItem.label}</h5>
+                <h5 className='text-sm'>{menuItem.label}</h5>
               </div>
             </div>
              
