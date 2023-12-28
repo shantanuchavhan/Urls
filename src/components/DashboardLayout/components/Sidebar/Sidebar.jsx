0f0369d6f1e58b7aae4050c27175a1de7be438fc
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-28 14:04:42 +0530

    commit

--- src/components/DashboardLayout/components/Sidebar/Sidebar.jsx
+++ src/components/DashboardLayout/components/Sidebar/Sidebar.jsx
@@ -15,16 +15,16 @@ const Sidebar = () => {
   const lastWord = currentPath.split('/').filter(Boolean).pop();
     const [activeSection,setActiveSection]=useState(lastWord||"")
     return (
-      <div className='flex flex-col  items-center justify-center py-3 gap-3 shadow-right text-gray-500 p-4'>
+      <div className='flex flex-col min-h-screen  items-center justify-center py-3 gap-3 shadow-right text-gray-500 p-4'>
         <div className='border-b-2 border-indigo-500 w-full text-center'>
           <h2>Menu</h2>
         </div>
-        <div className='flex flex-col  gap-3'>
+        <div className='flex flex-col  gap-2'>
           {dashboardSections.map((menuItem, index) => (
             <Link to={`/dashboard/${menuItem.route}`} key={index} onClick={()=>{
                 setActiveSection((old)=>menuItem.route)
             }}>
-            <div className='flex gap-2 items-center mb-2 relative'>
+            <div className='flex gap-2 items-center mb-1 relative'>
                 {activeSection === menuItem.route && <div className='h-3 w-3 absolute -left-5'><img src={active} alt="" /></div>}
                 <div className='flex items-center gap-2'>
                 <img className='h-4 w-4' src={menuItem.icon} alt='' />
@@ -43,4 +43,5 @@ const Sidebar = () => {
     );
   };
   
-  export default Sidebar;
+ 
+export default Sidebar;
